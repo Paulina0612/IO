b9e12421d95b29116b3fb24491435255cc57c7c6
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-03-31 15:40:18 +0200

    lab2-ex1-corrected

--- lab2/source_code/LAB2.py
+++ lab2/source_code/LAB2.py
@@ -7,6 +7,7 @@
 
 
 import cv2
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 import os
@@ -19,28 +20,72 @@ def Menu():
     return
 
 
+def writeASCII(header, filename, array):
+    with open(filename, 'w') as fh:
+        fh.write(header)
+        array.tofile(fh, sep=' ')
+        fh.write('\n')
+    return
+
+def writeBINARY(header, filename, array):
+    with open(filename, 'wb') as fh:
+        fh.write(bytearray(header, 'ascii'))
+        array.astype(np.uint8).tofile(fh)
+    return
+
 def ex1():
+    filenames = ['imgs\\ex1\\ascii_sketch_PPM.ppm', 
+                 'imgs\\ex1\\binary_sketch_PPM.ppm',
+                 'imgs\\ex1\\ascii_img_PPM.ppm', 
+                 'imgs\\ex1\\binary_img_PPM.ppm']
+    sketch = np.array([255, 0, 0, 0, 0, 255, 0, 255, 0, 255, 255, 0, 255, 0, 
+                       255, 0, 255, 255, 0, 0, 0, 255, 255, 255, 255, 0, 0])
+    
+    sketch_ascii_header = ('P3\n%d %d\n255\n')%(3, 3) 
+    sketch_binary_header = ('P6\n%d %d\n255\n')%(3, 3)
+
+    writeASCII(sketch_ascii_header, filenames[0], sketch.copy())
+    writeBINARY(sketch_binary_header, filenames[1], sketch.copy())
+    
+    sketch_from_ascii_file = cv2.imread(filenames[0])
+    sketch_from_bin_file = cv2.imread(filenames[1])
+    
+    fig, ax = plt.subplots(1,2)
+    ax[0].imshow(sketch_from_ascii_file)
+    ax[0].set_xlabel("Sketch from ascii file")
+    ax[1].imshow(sketch_from_bin_file)
+    ax[1].set_xlabel("Sketch from binary file")
+    plt.show()
+
     inFilename = 'imgs\\original_kitty.jpg'
 
     original = cv2.imread(inFilename)
-    original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
 
     array = np.asarray(original)
 
     height = len(array)
     width = len(array[0])
+
+    img_ascii_header = ('P3\n%d %d\n255\n')%(width, height) 
+    img_binary_header = ('P6\n%d %d\n255\n')%(width, height)
+
+    writeASCII(img_ascii_header, filenames[2], array.copy())
+    writeBINARY(img_binary_header, filenames[3], array.copy())
     
-    ppm_ascii_header = ('P3\n%d %d\n255\n')%(width, height) 
-    ppm_binary_header = ('P6\n%d %d\n255\n')%(width, height)
+    img_from_ascii_file = cv2.imread(filenames[2])
+    img_from_bin_file = cv2.imread(filenames[3])
+    
+    fig, ax = plt.subplots(1,2)
+    ax[0].imshow(img_from_ascii_file)
+    ax[0].set_xlabel("Image from ascii file")
+    ax[1].imshow(img_from_bin_file)
+    ax[1].set_xlabel("Image from binary file")
+    plt.show()
 
-    with open('imgs\\ex1\\asciiPPM.ppm', 'w') as fh:
-        fh.write(ppm_ascii_header)
-        array.tofile(fh, sep=' ')
-        fh.write('\n')
 
-    with open('imgs\\ex1\\binaryPPM.ppm', 'wb') as fh:
-        fh.write(bytearray(ppm_binary_header, 'ascii'))
-        array.tofile(fh)
+    print("Size of ASCII file is ", os.path.getsize(filenames[2])
+          , "and size of binary file is ", os.path.getsize(filenames[3])
+    )
 
     return
 
